66edf6a0dae86eefd3487d6b01cc0ec1e1a2cba5
LCAR979 <tcoperator@163.com>
2015-10-26 11:35:27 -0300

    empty project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HITCrs::Application.config.secret_token = '3d4b4c494ba46ea0ee8b125ce706767580b95f51394360b0a9c5a42e387fefdc54a3c36b0424709d49213fd9c4f05f85d492f9c9452bb035dc64f3f133377cbd'
